329da7ae184e3e5b2600b380862ac91cf583b83c
Navin Shrinivas <karupal2002@gmail.com>
2021-06-03 18:38:06 +0530

    breh

--- week5/1_1.c
+++ week5/1_1.c
@@ -1,25 +1,27 @@
 #include<stdio.h>
 
-void reversestring(char * rev, char * c) {
-  int n = 0;
-  while ( * (c + n) != '\0') {
-    n++;
-  }
-  for (int i = 0; i < n; i++)
-    *
-    (rev + n - i - 1) = * (c + i);
-  *(rev + n) = '\0';
+void reversestring(char* rev,char* c)
+{
+    int n=0;
+    while(*(c+n)!='\0'){
+        n++;
+    }
+    for(int i=0;i<n;i++)
+        *(rev+n-i-1)=*(c+i);
+    *(rev+n)='\0';
 }
-bool palindrome(char * c) {
-  int n = 0;
-  while ( * (c + n) != '\0') {
-    n++;
-  }
-  char rev[n];
-  reversestring(rev, c);
-  for (int i = 0; i < n; i++) {
-    if (rev[i] != c[i])
-      return false;
-  }
-  return true;
+bool palindrome(char* c)
+{
+    int n=0;
+    while(*(c+n)!='\0'){
+        n++;
+    }
+    char rev[n];
+    reversestring(rev,c);
+    for(int i=0;i<n;i++)
+    {
+        if(rev[i]!=c[i])
+            return false;
+    }
+    return true;
 }
